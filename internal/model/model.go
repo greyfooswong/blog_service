9130b08af2510b31e020e1d687b8d842393b7a13
southli <961065551@qq.com>
2021-11-30 19:35:49 +0800

    导入otgorm依赖

--- internal/model/model.go
+++ internal/model/model.go
@@ -4,6 +4,7 @@ import (
 	"blog-service/global"
 	"blog-service/pkg/setting"
 	"fmt"
+	otgorm "github.com/eddycjy/opentracing-gorm"
 	"github.com/jinzhu/gorm"
 	_ "github.com/jinzhu/gorm/dialects/mysql"
 	"time"
@@ -40,6 +41,7 @@ func NewDBEngine(databaseSetting *setting.DatabaseSettingS) (*gorm.DB, error) {
 	db.Callback().Delete().Replace("gorm:delete", deleteCallback)
 	db.DB().SetMaxIdleConns(databaseSetting.MaxIdleConns)
 	db.DB().SetMaxOpenConns(databaseSetting.MaxOpenConns)
+	otgorm.AddGormCallbacks(db)
 	return db, nil
 }
 
